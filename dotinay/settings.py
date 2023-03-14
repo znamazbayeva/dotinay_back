ffdbae2b2d8c2733d8536f0ab374cc5896308d2a
Samatqyzy <zhuldyznamazbayeva@gmail.com>
2023-03-14 19:32:47 +0600

    fix(settings): add allowed origins

--- dotinay/settings.py
+++ dotinay/settings.py
@@ -85,7 +85,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'dotinay.wsgi.application'
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = ['86.107.44.116']
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
