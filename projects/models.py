89fba7a02217e48b6d2c9d77bba5c69686348661
Samatqyzy <zhuldyznamazbayeva@gmail.com>
2023-04-09 02:18:18 +0600

    fix(project): add content

--- projects/models.py
+++ projects/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from about_me.models import Me
+from ckeditor.fields import RichTextField
 
 # Create your models here.
 class Project(models.Model):
@@ -7,6 +8,7 @@ class Project(models.Model):
   name = models.CharField(max_length=200, null=True, blank=True)
   company = models.CharField(max_length=200, null=True, blank=True)
   tools = models.CharField(max_length=200, null=True, blank=True)
-  description = models.CharField(max_length=200, null=True, blank=True)
+  description = models.TextField(null=True, blank=True)
+  content = RichTextField(null=True)
   url = models.TextField()
   photo = models.ImageField(upload_to="projects")
